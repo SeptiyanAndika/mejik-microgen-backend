316fe4c5af5547d342a62787d45f4be97d281f24
deceive3w <defrykun1@gmail.com>
2019-09-20 21:18:37 +0700

    add schema hooks

---
+++ schema/hooks/example.js
@@ -0,0 +1,30 @@
+module.exports = (app) => ({
+    before: {
+        get: async (context) => {
+        },
+        find: async (context) => {
+        },
+        create: async (context) => {
+        },
+        update: async (context) => {
+        },
+        delete: async (context) => {
+        },
+        patch: async (context) => {
+        }, 
+    },
+    after:{
+        get: async (context) => {
+        },
+        find: async (context) => {
+        },
+        create: async (context) => {
+        },
+        update: async (context) => {
+        },
+        delete: async (context) => {
+        },
+        patch: async (context) => {
+        }, 
+    },
+})
