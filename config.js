1ed2aa1c41312f22243ca790ce5310834a2dbad8
Adi Aswara <a.aswara@gmail.com>
2019-10-05 17:59:46 +0700

    delete schema

--- config.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    APP_NAME: process.env.APP_NAME,
-}
--- schema/services/user/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const User = require('./user')
-const ForgetPassword = require('./forget-password')
-const EmailVerification = require('./email-verification')
-module.exports =  {
-    User,
-    ForgetPassword,
-    EmailVerification
-}
